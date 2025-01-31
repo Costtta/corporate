b039dfd270be7261cf692510ec93408f7294e354
Ahmed Mohamed Ezzat <158809486+Costtta@users.noreply.github.com>
2025-01-31 07:40:13 +0200

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,41 +5,43 @@ import hero3 from './assets/img-hero3.avif'
 
 function DarkVariantExample() {
   return (
-    <Carousel id='home'>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={hero1}
-          alt="First slide"
-        />
-        <Carousel.Caption className='d-none d-md-block'>
-          <h5>The perfect design for your website</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={hero2}
-          alt="Second slide"
-        />
-        <Carousel.Caption className='d-none d-md-block'>
-          <h5>Start Your Future Financial Plan</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={hero3}
-          alt="Third slide"
-        />
-        <Carousel.Caption className='d-none d-md-block'>
-          <h5>Enjoy the Difference</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <section id='home' className='my-5 pt-3'>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={hero1}
+            alt="First slide"
+          />
+          <Carousel.Caption className='d-none d-md-block'>
+            <h5>The perfect design for your website</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={hero2}
+            alt="Second slide"
+          />
+          <Carousel.Caption className='d-none d-md-block'>
+            <h5>Start Your Future Financial Plan</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={hero3}
+            alt="Third slide"
+          />
+          <Carousel.Caption className='d-none d-md-block'>
+            <h5>Enjoy the Difference</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </section>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/corporate/', // Or '/' if deploying to your user page
 })
